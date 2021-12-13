43a65dbd53aaca606fe540af491acc423df756fa
david95j2 <glory20220j@gmail.com>
2021-12-13 21:17:34 +0900

    keras testing

--- src/test.py
+++ src/test.py
@@ -4,3 +4,6 @@ from keras.datasets import mnist
 
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
+network = models.Sequential()
+network.add(layers.Dense(512, activation='relu', input_shape-(28*28,)))
+network.add(layers.Dense(10, activation='softmax'))
