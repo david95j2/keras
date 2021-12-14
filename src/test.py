aac8188cf6d88ec0698c5685c9afdf11b15f4cee
david95j2 <glory20220j@gmail.com>
2021-12-14 14:56:14 +0900

    keras complie testing

--- src/test.py
+++ src/test.py
@@ -5,5 +5,8 @@ from keras.datasets import mnist
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
 network = models.Sequential()
-network.add(layers.Dense(512, activation='relu', input_shape-(28*28,)))
+network.add(layers.Dense(512, activation='relu', input_shape=(28*28,)))
 network.add(layers.Dense(10, activation='softmax'))
+
+# 코드 2-3 컴파일 단계
+network.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
