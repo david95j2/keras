ce5cb0545a773faa9e93e853f190080f61aca1f4
david95j2 <glory20220j@gmail.com>
2021-12-16 17:23:30 +0900

    keras testing

--- src/test.py
+++ src/test.py
@@ -10,3 +10,10 @@ network.add(layers.Dense(10, activation='softmax'))
 
 # 코드 2-3 컴파일 단계
 network.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# 코드 2-4 이미지 데이터 준비하기
+train_images = train_images.reshape((60000, 28 * 28))
+train_images = train_images.astype('float32') / 255
+
+test_images = test_images.reshape((10000, 28 * 28))
+test_images = test_images.astype('float32') / 255
