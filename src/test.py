0249dfb159bcfea9777c273fbb5512d14ea7473e
david95j2 <glory20220j@gmail.com>
2021-12-17 13:50:43 +0900

    about code testing

--- src/test.py
+++ src/test.py
@@ -17,3 +17,16 @@ train_images = train_images.astype('float32') / 255
 
 test_images = test_images.reshape((10000, 28 * 28))
 test_images = test_images.astype('float32') / 255
+
+
+# 코드 2-5 레이블 준비하기
+from keras.utils import to_categorical
+
+train_labels = to_categorical(train_labels)
+test_labels = to_categorical(test_labels)
+
+
+# 코드 : 훈련 실행과 결과
+network.fit(train_images, train_labels, epochs=5, batch_size=128)
+test_loss, test_acc = network.evaluate(test_images, test_labels)
+print('test_acc:', test_acc)
